2e7aa5a83a520b33b2f779d2f1ba7f0cf3a6414d
Danilo <danilokuzman1@yahoo.com>
2025-10-17 19:45:34 +0200

    Drustvena mreza Objavljivanje sadrzaja 2.A Dodavanje korisnika u grupu se belezi u bazi podataka

--- DrustveneMreze/Controllers/GroupMembersController.cs
+++ DrustveneMreze/Controllers/GroupMembersController.cs
@@ -1,63 +1,80 @@
 ﻿using DrustveneMreze.Domain;
 using DrustveneMreze.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace DrustveneMreze.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/groups/{groupId}/users/{userId}")]
     [ApiController]
     public class GroupMembersController : ControllerBase
     {
-        private GroupRepository groupRepository = new GroupRepository();
+        private readonly UserDbRepository userDbRepository;
+        private readonly GroupMembershipDbRepository GMDbRepository;
+        private readonly GroupDbRepository groupDbRepository;
 
-        [HttpPut("{groupId}")]
-        public ActionResult<User> Add(int groupId, int userId)
+        public GroupMembersController(IConfiguration configuration)
         {
-            if (!GroupRepository.Data.ContainsKey(groupId))
-            {
-                return NotFound();
-            }
-
-            if (!GroupRepository.Data.ContainsKey(userId))
-            {
-                return NotFound();
-            }
-
-            Group group = GroupRepository.Data[groupId];
-            User user = UserRepository.Data[userId];
-
-            if (!group.Users.Contains(user))
-            {
-                group.Users.Add(user);
-            }
-            return Ok(user);
+            GMDbRepository = new GroupMembershipDbRepository(configuration);
+            groupDbRepository = new GroupDbRepository(configuration);
+            userDbRepository = new UserDbRepository(configuration);
         }
 
-        [HttpDelete("{groupId}")]
-        public ActionResult<User> Delete(int groupId, int userId)
+        [HttpPut]
+        public ActionResult<Group> Add(int groupId, int userId)
         {
-
-            if (!GroupRepository.Data.ContainsKey(groupId))
+            Group group = groupDbRepository.GetById(groupId);
+            User user = userDbRepository.GetById(userId);
+            if (group == null)
             {
-                return NotFound();
-            }
-            if (!UserRepository.Data.ContainsKey(userId))
-            {
-                return NotFound();
-            }
-            Group group = GroupRepository.Data[groupId];
-            User user = UserRepository.Data[userId];
-
-            if (!group.Users.Contains(user))
-            {
-                return BadRequest();
+                return NotFound($"Grupa {groupId} nije pronadjena.");
             }
 
-            group.Users.Remove(user);
-            user.Groups.Remove(group);
-            groupRepository.Save();
-
-            return Ok(user);
+            if (user == null)
+            {
+                return NotFound($"Korisnik {userId} nije pronadjena.");
+            }
+            try
+            {
+                bool success = GMDbRepository.AddUser(groupId, userId);
+                if (!success)
+                {
+                    return Conflict($"Korisnik {userId} vec postoji u grupi.");
+                }
+                return Ok(group);
+            }
+            catch(Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
+
+        //[HttpDelete("{groupId}")]
+        //public ActionResult<User> Delete(int groupId, int userId)
+        //{
+
+        //    if (!GroupRepository.Data.ContainsKey(groupId))
+        //    {
+        //        return NotFound();
+        //    }
+        //    if (!UserRepository.Data.ContainsKey(userId))
+        //    {
+        //        return NotFound();
+        //    }
+        //    Group group = GroupRepository.Data[groupId];
+        //    User user = UserRepository.Data[userId];
+
+        //    if (!group.Users.Contains(user))
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    group.Users.Remove(user);
+        //    user.Groups.Remove(group);
+        //    groupRepository.Save();
+
+        //    return Ok(user);
+        //}
     }
 }
---
+++ DrustveneMreze/Repositories/GroupMembershipDbRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace DrustveneMreze.Repositories
+{
+    public class GroupMembershipDbRepository
+    {
+        private readonly string connectionStr;
+
+        public GroupMembershipDbRepository(IConfiguration configuration)
+        {
+            connectionStr = configuration["ConnectionString:SQLiteConnection"];
+        }
+
+
+
+        public bool AddUser(int groupId, int userId)
+        {
+            try
+            {
+                using SqliteConnection connection = new SqliteConnection(connectionStr);
+                connection.Open();
+
+                var checkCommand = connection.CreateCommand();
+                checkCommand.CommandText = @"SELECT COUNT(*) FROM GroupMemberships WHERE UserId = @UserId AND GroupId = @GroupId";
+
+                checkCommand.Parameters.AddWithValue("@GroupId", groupId);
+                checkCommand.Parameters.AddWithValue("@UserId", userId);
+
+                long i = (long)checkCommand.ExecuteScalar();
+                if(i > 0)
+                {
+                    return false;
+                }
+
+                string query = "INSERT INTO GroupMemberships (GroupId, UserId) VALUES (@GroupId, @UserId);";
+                using SqliteCommand command = new SqliteCommand(query, connection);
+
+                command.Parameters.AddWithValue("@GroupId", groupId);
+                command.Parameters.AddWithValue("@UserId", userId);
+
+                int affectedRows = command.ExecuteNonQuery();
+                return affectedRows > 0;
+            }
+
+            catch (SqliteException ex)
+            {
+                Console.WriteLine($"Greška pri povezivanju sa bazom ili izvršavanju SQL upita: {ex.Message}");
+                throw;
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Greška jer konekcija nije ili je više puta otvorena: {ex.Message}");
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Neočekivana greška: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
